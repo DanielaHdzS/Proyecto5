1b4aef3f3bdd65bfab4637622b12adaa9fd35f61
DanielaHdzS <daniela.hdez1510@gmail.com>
2023-12-25 21:04:24 -0600

    Subir proyecto

---
+++ app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pandas as pd
+import plotly_express as px
+
+car_data = pd.read_csv(
+    'D:/Python/Tripleten/Proyectos/Sprint5/vehicles_us.csv')  # leer los datos
+
+st.header('Aplicacion Proyecto Sprint 5')
+
+build_histogram = st.checkbox('Construir histograma')  # crear un botón
+
+if build_histogram:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+build_scatter = st.checkbox(
+    'Construir grafico de dispersion')  # crear un botón
+
+if build_scatter:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un grafico de dispersion para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un histograma
+    fig = px.scatter(car_data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
